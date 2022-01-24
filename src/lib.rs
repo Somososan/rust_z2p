3666af7142b0f6732a387b02526f6008a29f767d
Chrysostomos Karaliolios <ckaraliolios@gmail.com>
2022-01-24 14:45:35 +0100

    restructerd source folder

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,10 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn hello_world() -> impl Responder {
-    String::from("Hello World")
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    name: String,
-    email: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(hello_world))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
+    startup::start(listener)
 }
