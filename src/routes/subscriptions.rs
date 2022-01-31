ebc847ae8171d4b2318358f0f0a55c9fcb522579
Chrysostomos Karaliolios <ckaraliolios@gmail.com>
2022-01-31 16:11:01 +0100

    clippy allow async_yields_async

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,17 +10,17 @@ pub struct FormData {
     email: String,
 }
 
+#[allow(clippy::async_yields_async)]
 #[tracing::instrument(
     name="Adding a new subsciber",
-    skip(form,pool), 
+    skip(form,pool),
     fields(
         subscriber_email=%form.email,
         subscriber_name=%form.name
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&form,&pool).await
-    {
+    match insert_subscriber(&form, &pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
             tracing::error!("Failed to execute query: {:?}", e);
@@ -33,7 +33,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     name = "Saving new subscriber details in the database",
     skip(form, pool)
 )]
-pub async fn insert_subscriber(form: &FormData, pool: &PgPool)->Result<(),sqlx::Error> {
+pub async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -46,7 +46,9 @@ pub async fn insert_subscriber(form: &FormData, pool: &PgPool)->Result<(),sqlx::
     )
     .execute(pool)
     .await
-    .map_err(|e| {tracing::error!("Failed to execute query: {:?}", e); e})?;
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
     Ok(())
-    
 }
