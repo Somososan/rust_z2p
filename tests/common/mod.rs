daed5f91be386336966aa0c5fc037987641c5ae2
Chrysostomos Karaliolios <ckaraliolios@gmail.com>
2022-02-07 16:31:08 +0100

    split integration tests

---
+++ tests/common/mod.rs
@@ -0,0 +1,56 @@
+use once_cell::sync::Lazy;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use std::net::TcpListener;
+use uuid::Uuid;
+use zero2prod::configuration::{get_configuration, DataBaseSettings};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info";
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
+
+pub async fn spawn_app() -> (String, PgPool) {
+    Lazy::force(&TRACING);
+
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to allocate local port");
+    let port = listener.local_addr().unwrap().port();
+
+    let mut configuration = get_configuration().expect("Failed to read configuration");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let db_pool = configure_database(&configuration.database).await;
+
+    let server = zero2prod::run(listener, db_pool.clone()).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    (format!("http://127.0.0.1:{}", port), db_pool)
+}
+
+pub async fn configure_database(config: &DataBaseSettings) -> PgPool {
+    let mut connection = PgConnection::connect_with(&config.without_db())
+        .await
+        .expect("Failed to connect to postgres");
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create DATABASE");
+
+    let connection_pool = PgPool::connect_with(config.with_db())
+        .await
+        .expect("failed to connect to database");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("");
+
+    connection_pool
+}
