00d79eb4e0866bab68a4d8a7022407d6ef37ef7e
Chrysostomos Karaliolios <ckaraliolios@gmail.com>
2022-02-10 12:31:05 +0100

    added email client base || till 7.2.2.4 z2p

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::domain::SubscriberEmail;
+use zero2prod::email_client::EmailClient;
 use zero2prod::run;
 use zero2prod::telemetry;
 
@@ -14,7 +16,12 @@ async fn main() -> std::io::Result<()> {
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
 
+    let email_client = EmailClient::new(
+        "/".to_string(),
+        SubscriberEmail::parse("sender@email.com".to_string()).unwrap(),
+    );
+
     let listener = TcpListener::bind(configuration.application.address())
         .expect("Failed to allocate local port");
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
